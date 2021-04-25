205f99a6d70b689b51d04f26e58fc1f7f9f71147
Piotr Markielau <pmarkielau@gmail.com>
2021-04-26 01:34:16 +0300

    Remove scripts

--- restart.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-docker stop cryptosniffer_app && docker container rm cryptosniffer_app
-docker build -t cryptosniffer_app_image .
-docker run --name cryptosniffer_app --network="host" -v "$(pwd)/docker":/var cryptosniffer_app_image
